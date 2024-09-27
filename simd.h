45c089ad484404e7dc714912391a66c59445e794
0x4A4D00 <40355105+0x4A4D00@users.noreply.github.com>
2024-09-27 20:43:05 +0330

    Base Files Added

---
+++ simd.h
@@ -0,0 +1,67 @@
+/*
+ *  Part of SIMD functions implementation of lx7 architecture for ESP32S3.
+ *  Author: 0x4A4D
+ */
+
+extern "C" {
+/* Add Packed 8-bit Values (16)  
+ * EE.VADDS.S8  Q3, Q1, Q2
+ */   
+  int8_t* m8_add_p16(int8_t *A, int8_t *B, int8_t *C);
+
+/* Add Packed 16-bit Values (8)  
+ * EE.VADDS.S16  Q3, Q1, Q2
+ */ 
+  in16_t* m16_add_p8(in16_t *A, in16_t *B, in16_t *C);
+
+/* Add Packed 32-bit Values (4)  
+ * EE.VADDS.S32  Q3, Q1, Q2
+ */ 
+  in32_t* m32_add_p4(in32_t *A, in32_t *B, in32_t *C);
+
+
+
+
+/* Subtraction of two Packed Arrays with 8-bit Values (16)  
+ * EE.VSUBS.S8  Q3, Q1, Q2
+ */   
+  int8_t* m8_sub_p16(int8_t *A, int8_t *B, int8_t *C);
+
+/* Add Packed 16-bit Values (8)  
+ * EE.VSUBS.S16  Q3, Q1, Q2
+ */ 
+  in16_t* m16_sub_p8(in16_t *A, in16_t *B, in16_t *C);
+
+/* Add Packed 32-bit Values (4)  
+ * EE.VSUBS.S32  Q3, Q1, Q2
+ */ 
+  in32_t* m32_sub_p4(in32_t *A, in32_t *B, in32_t *C);
+
+
+
+
+/* Signed multiplication of two Packed Arrays with 8-bit Values (16)  
+ * EE.VMUL.S8  Q3, Q1, Q2
+ */   
+  int16_t* m8_mul_p16(int8_t *A, int8_t *B, int8_t *C);
+
+/* Signed multiplication of two Packed Arrays with 16-bit Values (8)  
+ * EE.VMUL.S16  Q3, Q1, Q2
+ */ 
+  in32_t* m16_mul_p8(in16_t *A, in16_t *B, in16_t *C);
+
+
+
+
+/* Unsigned multiplication of two Packed Arrays with 8-bit Values (16)  
+ * EE.VMUL.U8  Q3, Q1, Q2
+ */   
+  int16_t* m8_umul_p16(int8_t *A, int8_t *B, int8_t *C);
+
+/* Unsigned multiplication of two Packed Arrays with 16-bit Values (8)  
+ * EE.VMUL.U16  Q3, Q1, Q2
+ */ 
+  in32_t* m16_umul_p8(in16_t *A, in16_t *B, in16_t *C);
+
+}
+
